d60c37388fdfeb00a8ae96501bc0a0a29db7b46c
Shane Hofstetter <shane.hofstetter@gmail.com>
2018-06-16 00:17:03 +0200

    implement basic i18n text resolve on hover

--- src/i18nHoverProvider.ts
+++ src/i18nHoverProvider.ts
@@ -1,24 +1,22 @@
-import { CancellationToken, Hover, HoverProvider, Position, TextDocument, workspace } from 'vscode';
+import { CancellationToken, Hover, HoverProvider, Position, TextDocument } from 'vscode';
 import { i18nResolver } from './extension';
 
 export class I18nHoverProvider implements HoverProvider {
 
-    public provideHover(document: TextDocument, position: Position, token: CancellationToken): Thenable<Hover> {
-        console.log("provideHover", document, position);
-        
-        // 1. get i18n key of current location
-
+    public provideHover(document: TextDocument, position: Position, token: CancellationToken): Hover {
         let lineText = document.lineAt(position.line).text;
-        let afterI18nCall = lineText.split(/I18n\.t\(| t\(/)[1]
-        let i18nKey = afterI18nCall.split(/\ |\,|\)/)[0].replace(/'|\"/g, "")
-        console.log("i18nKey", i18nKey);
 
-        // 2. ask resolver for its value
-        
-        let result = i18nResolver.getLocaleForKey(i18nKey);
-        console.log("result", result);
-        // 3. return hover definition
+        let range = i18nResolver.getRangeOfI18nKeyAtPosition(position, document);
+        if (!range) {
+            return null;
+        }
+        let i18nKey = i18nResolver.getI18nKeyAtRangeFromDocument(range, document);
+        if (!i18nResolver.isValidI18nKey(i18nKey)) {
+            return null;
+        }
 
-        return null;
+        let i18nText = i18nResolver.getLocaleForKey(i18nResolver.makeAbsoluteKey(i18nKey, document.fileName));
+
+        return new Hover({ language: 'text', value: i18nText }, range);
     }
 }
--- src/i18nResolver.ts
+++ src/i18nResolver.ts
@@ -1,49 +1,127 @@
-import { workspace } from "vscode";
-import * as vscode from 'vscode';
 import { safeLoad } from "js-yaml";
 import * as merge from "merge";
+import * as vscode from 'vscode';
+import { Position, Range, TextDocument, workspace } from 'vscode';
 
 export class I18nResolver {
 
     private locales;
     private defaultLocale = "en";
+    private fileSystemWatcher;
+    private readonly yamlPattern = 'config/locales/**/*.yml';
 
     /**
      * generates map of locales
      */
     public loadYamlFiles() {
-        // TODO populate locales
-        console.log(workspace.rootPath);
+        if (this.locales) {
+            return;
+        }
         this.locales = {};
-        workspace.findFiles('config/locales/**/*.yml').then(files => {
-            console.log("files", files);
+        workspace.findFiles(this.yamlPattern).then(files => {
             files.forEach(file => {
                 workspace.openTextDocument(file.path).then((document: vscode.TextDocument) => {
                     this.locales = merge.recursive(false, this.locales, safeLoad(document.getText()));
                 });
             });
         });
+
+        this.getDefaultLocale().then(locale => { this.defaultLocale = locale; });
+
+        this.fileSystemWatcher = workspace.createFileSystemWatcher(new vscode.RelativePattern(workspace.rootPath, this.yamlPattern));
+        this.fileSystemWatcher.onDidChange((e: vscode.Uri) => {
+            workspace.openTextDocument(e.fsPath).then((document: vscode.TextDocument) => {
+                this.locales = merge.recursive(false, this.locales, safeLoad(document.getText()));
+            });
+        });
     }
 
-    public getDefaultLocale(): Thenable<string> {
+    /**
+     * get the default locale configured in application.rb
+     * @returns default locale key or 'en' as fallback if default cant be found
+     */
+    private getDefaultLocale(): Thenable<string> {
         return workspace.openTextDocument(`${workspace.rootPath}/config/application.rb`).then((document: vscode.TextDocument) => {
             let searchResult = document.getText().search(/i18n\.default_locale/g);
+            if (!searchResult) {
+                return "en";
+            }
             let position = document.positionAt(searchResult);
             let lineText = document.lineAt(position.line).text;
             let locale = lineText.split("=")[1].replace(/\:|\ |\'|\"/g, "").trim();
-            console.log("default locale", locale);
             return locale;
         });
     }
 
     /**
-     * get value for i18n key
+     * resolve text value for i18n key in default locale
      * @param key i18n key (e.g. "hello.world")
      */
-    public getLocaleForKey(key: string): string | null {
-        // TODO find key in locales and return value
-        this.getDefaultLocale().then(locale => { this.defaultLocale = locale; });
-        
-        return null;
+    public getLocaleForKey(key: string): string | null | undefined {
+        if (!key) {
+            return null;
+        }
+
+        let currentLocaleMap = this.locales;
+        let keys = key.split(".");
+        keys.unshift(this.defaultLocale);
+        keys = keys.filter(key => key.length > 0);
+        keys.forEach(keyPart => {
+            if (currentLocaleMap !== undefined) {
+                currentLocaleMap = currentLocaleMap[keyPart];
+            }
+        });
+
+        if (currentLocaleMap !== null && typeof currentLocaleMap === "object") {
+            let resultLines = [];
+            Object.keys(currentLocaleMap).forEach(key => {
+                let text = currentLocaleMap[key];
+                if (typeof text === 'object') {
+                    return null;
+                }
+                resultLines.push(`${key}: ${text}`);
+            });
+            return resultLines.join("\n");
+        }
+
+        return currentLocaleMap;
+    }
+
+    public makeAbsoluteKey(key: string, currentFilename: string): string {
+        if (!key.startsWith(".")) {
+            return key;
+        }
+        try {
+            // get the relative key from current file path, starting at directory "views"
+            let relativeKey = currentFilename.split("views")[1].split(".")[0].replace(/\\|\//g, ".");
+            if (relativeKey.startsWith(".")) {
+                relativeKey = relativeKey.substring(1);
+            }
+
+            // remove all underlines at beginning of key parts
+            let relativeKeyParts = relativeKey.split(".");
+            relativeKeyParts = relativeKeyParts.map(keyPart => {
+                if (keyPart.startsWith("_")) {
+                    return keyPart.substring(1);
+                }
+                return keyPart;
+            });
+            return relativeKeyParts.join(".") + key;
+        } catch (Error) {
+            return key;
+        }
+    }
+
+    public isValidI18nKey(key: string) {
+        return key.indexOf(".") >= 0;
+    }
+
+    public getRangeOfI18nKeyAtPosition(position: Position, document: TextDocument): Range {
+        let i18nCallRegex = /(I18n\.)?t[\(\s]+[\"\']{1}[\w+\.\/]+[\"\'\.]{1}\)?/g;
+        return document.getWordRangeAtPosition(position, i18nCallRegex);
+    }
+
+    public getI18nKeyAtRangeFromDocument(range: Range, document: TextDocument): string {
+        return document.getText(range).replace(/\"|\'|(I18n\.)?t[\(\s]+|\)/g, "");
     }
 }
