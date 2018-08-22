e0a164a79d7486a36c2f10ccf701d03abbc36f51
Shane Hofstetter <shane.hofstetter@gmail.com>
2018-08-22 12:51:58 +0200

    add slim language support

--- src/extension.ts
+++ src/extension.ts
@@ -11,15 +11,16 @@ export function activate(context: vscode.ExtensionContext) {
 
     i18nResolver.load();
 
-    const HAML = { language: 'haml', scheme: 'file' };
-    const ERB = { language: 'erb', scheme: 'file' };
+    const documentFilters = [
+        { language: 'haml', scheme: 'file' },
+        { language: 'erb', scheme: 'file' },
+        { language: 'slim', scheme: 'file' }
+    ];
 
-    context.subscriptions.push(vscode.languages.registerHoverProvider(HAML, new I18nHoverProvider()));
-    context.subscriptions.push(vscode.languages.registerHoverProvider(ERB, new I18nHoverProvider()));
+    context.subscriptions.push(vscode.languages.registerHoverProvider(documentFilters, new I18nHoverProvider()));
 
     const triggerCharacters = ".abcdefghijklmnopqrstuvwxyz".split("");
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(HAML, new I18nCompletionProvider(), ...triggerCharacters));
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(ERB, new I18nCompletionProvider(), ...triggerCharacters));
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(documentFilters, new I18nCompletionProvider(), ...triggerCharacters));
 }
 
 export function deactivate() {
