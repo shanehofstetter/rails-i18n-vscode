18f00b615115941feaa3007e3095fb95b84a54ca
Shane Hofstetter <shane.hofstetter@gmail.com>
2018-08-25 01:54:18 +0200

    add logging

--- src/i18nHoverProvider.ts
+++ src/i18nHoverProvider.ts
@@ -1,6 +1,7 @@
 import { CancellationToken, Hover, HoverProvider, Position, TextDocument } from 'vscode';
 import { i18nResolver } from './extension';
 import { I18nKeyDetector } from './i18nKeyDetector';
+import { logger } from './logger';
 
 export class I18nHoverProvider implements HoverProvider {
 
@@ -10,11 +11,13 @@ export class I18nHoverProvider implements HoverProvider {
             return null;
         }
         let i18nKey = I18nKeyDetector.getI18nKeyAtRangeFromDocument(range, document);
+        logger.debug('provideHover', `i18nkey: '${i18nKey}'`);
         if (!I18nKeyDetector.isValidI18nKey(i18nKey)) {
             return null;
         }
 
         let i18nText = i18nResolver.getTranslationForKey(I18nKeyDetector.makeAbsoluteKey(i18nKey, document.fileName));
+        logger.debug('provideHover', `i18nText: '${i18nText}'`);
 
         return new Hover({ language: 'text', value: i18nText }, range);
     }
