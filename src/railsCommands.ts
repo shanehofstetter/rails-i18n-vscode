32a146ddb58e8ef6ab2aaef748ff98733e27ccb7
Shane Hofstetter <shane.hofstetter@gmail.com>
2018-08-26 20:15:01 +0200

    add method to get paths of all translation files

--- src/railsCommands.ts
+++ src/railsCommands.ts
@@ -6,11 +6,19 @@ export class RailsCommands {
     public static getDefaultLocale(workspaceFolder: WorkspaceFolder): Promise<string> {
         return RailsCommands.callRails(['runner', 'puts "default_locale=#{I18n.default_locale}"'], workspaceFolder).then((result) => {
             const defaultLocale: string = result.split('=')[1].trim();
-            logger.debug('default locale from rails:', defaultLocale);
+            logger.debug('default locale from rails:', defaultLocale, 'workspace dir:', workspaceFolder.name);
             if (defaultLocale) {
                 return Promise.resolve(defaultLocale);
             }
             return Promise.reject();
+        });
+    }
+
+    public static getLoadPaths(workspaceFolder: WorkspaceFolder): Promise<string[]> {
+        return RailsCommands.callRails(['runner', 'I18n.load_path.each { |path| puts path }'], workspaceFolder).then((result) => {
+            const translationFiles: string[] = result.split(/[\r\n]+/).filter(line => line.endsWith('.yml'));
+            logger.debug('translation files: ', translationFiles);
+            return Promise.resolve(translationFiles);
         });
     }
 
@@ -20,7 +28,7 @@ export class RailsCommands {
 
     private static callRails(args: string[], workspaceFolder: WorkspaceFolder): Promise<string> {
         const railsCommand: string[] = RailsCommands.getRailsCommand();
-        logger.debug('callRails', railsCommand, args, workspace);
+        logger.debug('callRails', railsCommand, args, workspaceFolder);
         return execa(railsCommand.shift(), [...railsCommand, ...args], {
             cwd: workspaceFolder.uri.path
         }).then(result => {
@@ -30,7 +38,7 @@ export class RailsCommands {
             logger.warn('rails command returned error:', railsCommand, args, result.code, result.stderr);
             return Promise.reject(result.stderr);
         }, error => {
-            logger.error('rails command failed:', error.message);
+            logger.error('rails command failed:', error.message, 'workspace dir:', workspaceFolder.name);
             return Promise.reject(error.message);
         });
     }
