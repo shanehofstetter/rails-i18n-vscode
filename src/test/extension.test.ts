ec5e562537e6ee6c372a341ccd371d9749e347e8
Shane Hofstetter <shane.hofstetter@gmail.com>
2018-09-23 22:16:53 +0200

    disable extension in test, activate manually

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,10 +1,9 @@
-import { workspace, Uri } from "vscode";
+import { workspace, Uri, ExtensionContext } from "vscode";
 import * as path from 'path';
 import * as assert from 'assert';
-import { i18nResolver } from "../extension";
+import { i18nResolver, activate } from "../extension";
 import * as fs from 'fs-extra';
 import { i18nTree } from "../i18nTree";
-import logger = require('../logger');
 
 const fixturePath = path.join(__dirname, "..", "..", "src", "test", 'fixtures');
 const translations = {
@@ -17,27 +16,39 @@ const translations = {
 }
 
 describe("Extension", () => {
-    logger.logLevel = logger.LogLevel.Debug;
-
     beforeEach(() => {
-        workspace.updateWorkspaceFolders(0, 0, { uri: Uri.file(path.join(__dirname)), name: 'test' });
         fs.copySync(fixturePath, __dirname);
+        workspace.updateWorkspaceFolders(0, 0, { uri: Uri.file(path.join(__dirname)), name: 'test' });
     });
 
-    it('does load translations on activation and returns translation for existing key', function (done) {
-        this.timeout(20000);
+    describe('activation', () => {
+        let mockContext: ExtensionContext = {
+            subscriptions: [],
+            workspaceState: null,
+            globalState: null,
+            extensionPath: '',
+            asAbsolutePath: null,
+            storagePath: null
+        }
 
-        let viewFile = Uri.file(path.join(__dirname, 'app', 'views', 'blog', 'show.html.haml'));
-        workspace.openTextDocument(viewFile).then(() => {
-            i18nResolver.onDidLoad(() => {
-                Object.keys(translations).forEach(locale => {
-                    assert.equal(i18nResolver.getTranslationForKey('hello', locale, viewFile), translations[locale].hello);
-                    assert.equal(i18nTree.lookupKey(`test.${locale}.hello`), translations[locale].hello);
-                    assert.equal(i18nTree.getTranslation('hello', locale, 'test'), translations[locale].hello);
-                    assert.equal(i18nTree.translationsForLocaleExist(locale, 'test'), true);
+        beforeEach(() => {
+            activate(mockContext);
+            assert.equal(mockContext.subscriptions.length, 4);
+        });
+
+        it('does load translations and returns translation for existing key', function (done) {
+            let viewFile = Uri.file(path.join(__dirname, 'app', 'views', 'blog', 'show.html.haml'));
+            workspace.openTextDocument(viewFile).then(() => {
+                i18nResolver.onDidLoad(() => {
+                    Object.keys(translations).forEach(locale => {
+                        assert.equal(i18nResolver.getTranslationForKey('hello', locale, viewFile), translations[locale].hello);
+                        assert.equal(i18nTree.lookupKey(`test.${locale}.hello`), translations[locale].hello);
+                        assert.equal(i18nTree.getTranslation('hello', locale, 'test'), translations[locale].hello);
+                        assert.equal(i18nTree.translationsForLocaleExist(locale, 'test'), true);
+                    });
+                    assert.equal(i18nTree.translationsForLocaleExist('it', 'test'), false);
+                    done();
                 });
-                assert.equal(i18nTree.translationsForLocaleExist('it', 'test'), false);
-                done();
             });
         });
     });
